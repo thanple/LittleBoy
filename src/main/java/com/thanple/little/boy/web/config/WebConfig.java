3590fa5b6ece363750c1875940ec2957a74a77e6
null47 <805685524@qq.com>
2018-05-27 19:28:46 +0800

    springboot自定义xml进行依赖注入

--- src/main/java/com/thanple/little/boy/web/config/WebConfig.java
+++ src/main/java/com/thanple/little/boy/web/config/WebConfig.java
@@ -6,12 +6,9 @@ import org.springframework.boot.web.servlet.ErrorPageRegistrar;
 import org.springframework.boot.web.servlet.ErrorPageRegistry;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
@@ -21,6 +18,13 @@ import javax.servlet.http.HttpServletResponse;
  * Created by Thanple on 2018/5/23.
  */
 @Configuration
+/**
+ * ImportResource引入资源文件有三种方式：
+ *     1.直接引入，该路径就是src/resources/下面的文件：file
+ *     2.classpath引入：该路径就是src/java下面的配置文件：classpath:file
+ *     3.引入本地文件：该路径是一种绝对路径：file:D://....
+ */
+@ImportResource(locations = {"spring-context.xml"}) //这里引入自定义的bean
 public class WebConfig {
 
     @SuppressWarnings("SpringJavaAutowiringInspection")
---
+++ src/main/java/com/thanple/little/boy/web/entity/StartBean.java
@@ -0,0 +1,37 @@
+package com.thanple.little.boy.web.entity;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Create by Thanple at 2018/5/27 下午7:18
+ * 这个bean是为了测试spring-context.xml文件中配置的bean,spring容器启动的时候就会加载这个bean
+ */
+public class StartBean {
+
+    private static Logger log = LoggerFactory.getLogger(StartBean.class);
+
+    private String initAttr;
+
+    static {
+        log.info("Startbean Class init");
+    }
+
+    public StartBean(String initAttr) {
+        this.initAttr = initAttr;
+        log.info("Startbean construct initAttr={}",initAttr);
+    }
+
+    public StartBean() {
+        log.info("Startbean construct ...");
+    }
+
+    public String getInitAttr() {
+        return initAttr;
+    }
+
+    public void setInitAttr(String initAttr) {
+        this.initAttr = initAttr;
+        log.info("Startbean set initAttr={}",initAttr);
+    }
+}
